857614c990e5e5a717d62e1b9a02ff63f9397769
Ricardo Galli <gallir@raspberrypi.(none)>
2015-02-21 09:43:28 +0100

    added spinlosk con sched_yield

---
+++ counter_spinlock.c
@@ -0,0 +1,65 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+
+
+#define NUM_THREADS	     4
+#define MAX_COUNT 10000000
+
+// Just used to send the index of the id
+struct tdata {
+	int tid;
+};
+
+// Used for swapping in __sync_bool_compare_and_swap
+int mutex = 0;
+
+int counter = 0;
+
+void lock(int i) {
+	while(mutex || ! __sync_bool_compare_and_swap(&mutex, 0, 1)) {
+		sched_yield();
+	}
+}
+
+void unlock(int i) {
+	mutex = 0;
+}
+	
+void *count(void *ptr) {
+	long i, max = MAX_COUNT/NUM_THREADS;
+	int tid = ((struct tdata *) ptr)->tid;
+
+	for (i=0; i < max; i++) {
+		lock(tid);
+		counter += 1; // The global variable, i.e. the critical section
+		unlock(tid);
+	}
+
+	printf("End %d counter: %d\n", tid, counter);
+	pthread_exit(NULL);
+}
+
+int main (int argc, char *argv[]) {
+	pthread_t threads[NUM_THREADS];
+	int rc, i;
+	struct tdata id[NUM_THREADS];
+
+	for(i=0; i<NUM_THREADS; i++){
+		id[i].tid = i;
+		rc = pthread_create(&threads[i], NULL, count, (void *) &id[i]);
+		if (rc){
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			exit(-1);
+		}
+	}
+
+	for(i=0; i<NUM_THREADS; i++){
+		pthread_join(threads[i], NULL);
+	}
+
+	printf("Counter value: %d Expected: %d\n", counter, MAX_COUNT);
+	return 0;
+}
+
