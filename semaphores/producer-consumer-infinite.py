cb663673d4cb6ca4a6bfc9cfee88457abbd9fd0c
Ricardo Galli <gallir@gmail.com>
2015-04-06 15:06:24 +0200

    Producer-consumers

---
+++ semaphores/producer-consumer-infinite.py
@@ -0,0 +1,63 @@
+#! /usr/bin/env python
+
+import threading
+import time
+
+TO_PRODUCE = 1000
+PRODUCERS = 2
+CONSUMERS = 2
+
+buffer = []
+mutex = threading.RLock()
+notEmpty = threading.Semaphore(0)
+
+
+class Producer(threading.Thread):
+    def run(self):
+        global counter
+        myName = threading.currentThread().getName()
+        print("Producer {}".format(myName))
+
+        for i in range(TO_PRODUCE):
+            data = "{} i: {}".format(myName, i)
+            with mutex:
+                buffer.append(data)
+            notEmpty.release()
+
+
+class Consumer(threading.Thread):
+    def run(self):
+        global counter
+        myName = threading.currentThread().getName()
+        print("Consumer {}".format(myName))
+
+        for i in range(TO_PRODUCE):
+            notEmpty.acquire()
+            with mutex:
+                data = buffer.pop(0)
+                print("{} read: {}".format(myName, data))
+
+def main():
+    threads = []
+
+    for i in range(CONSUMERS):
+        c = Consumer()
+        threads.append(c)
+
+    for i in range(PRODUCERS):
+        p = Producer()
+        threads.append(p)
+
+    # Start all threads
+    for t in threads:
+        t.start()
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+
+    print("End")
+
+
+if __name__ == "__main__":
+    main()
---
+++ semaphores/producer-consumer.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python
+
+import threading
+import time
+
+TO_PRODUCE = 1000
+BUFFER_SIZE = 10
+PRODUCERS = 2
+CONSUMERS = 2
+
+buffer = []
+mutex = threading.RLock()
+notFull = threading.Semaphore(BUFFER_SIZE)
+notEmpty = threading.Semaphore(0)
+
+
+class Producer(threading.Thread):
+    def run(self):
+        global counter
+        myName = threading.currentThread().getName()
+        print("Producer {}".format(myName))
+
+        for i in range(TO_PRODUCE):
+            data = "{} i: {}".format(myName, i)
+            notFull.acquire()
+            with mutex:
+                buffer.append(data)
+            notEmpty.release()
+            time.sleep(0.01)
+
+class Consumer(threading.Thread):
+    def run(self):
+        global counter
+        myName = threading.currentThread().getName()
+        print("Consumer {}".format(myName))
+
+        for i in range(TO_PRODUCE):
+            notEmpty.acquire()
+            with mutex:
+                data = buffer.pop(0)
+                print("{} read: {}".format(myName, data))
+            notFull.release()
+
+def main():
+    threads = []
+
+    for i in range(CONSUMERS):
+        c = Consumer()
+        threads.append(c)
+        c.start();
+
+    for i in range(PRODUCERS):
+        p = Producer()
+        threads.append(p)
+        p.start();
+
+    # Wait for all threads to complete
+    for t in threads:
+        t.join()
+
+    print("End")
+
+
+if __name__ == "__main__":
+    main()
