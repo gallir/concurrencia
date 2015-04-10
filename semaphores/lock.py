b444c258bf580bdd442bcb0e8ed3213ed35e206e
Ricardo Galli <gallir@gmail.com>
2015-04-10 23:41:44 +0200

    Unified to static class variable

--- semaphores/lock.py
+++ semaphores/lock.py
@@ -6,11 +6,11 @@ import time
 THREADS = 4
 MAX_COUNT = 10000000
 
-counter = 0
 
 
 class MyThread(threading.Thread):
     mutex = threading.Lock()
+    counter = 0
     def __init__(self, threadID):
         super(MyThread, self).__init__()
         self.threadID = threadID
@@ -21,7 +21,7 @@ class MyThread(threading.Thread):
 
         for i in range(MAX_COUNT/THREADS):
             MyThread.mutex.acquire()
-            counter += 1
+            MyThread.counter += 1
             MyThread.mutex.release()
 
 def main():
@@ -39,7 +39,7 @@ def main():
     for t in threads:
         t.join()
 
-    print("Counter value: %d Expected: %d\n" % (counter, MAX_COUNT))
+    print("Counter value: %d Expected: %d\n" % (MyThread.counter, MAX_COUNT))
 
 
 if __name__ == "__main__":
