90e8cbe21f382096f48e4e012fa077ed6f814507
Ricardo Galli <gallir@gmail.com>
2015-04-21 03:15:52 +0200

    Moved to an unqie instance across all threads

--- monitors/Counter.java
+++ monitors/Counter.java
@@ -1,20 +1,15 @@
 public class Counter implements Runnable {
     static final int THREADS = 4;
     static final int MAX_COUNT = 10000000;
-    static volatile int counter = 0;
-    int id;
 
-    // It should be static because every thread has a different Counter instance
-    static Object lock = new Object();
+    public volatile int counter = 0;
+    Object lock = new Object();
 
-    public Counter(int id) {
-        this.id = id;
-    }
 
     @Override
     public void run() {
         int max = MAX_COUNT/THREADS;
-        System.out.printf("Thread %d\n", id);
+        System.out.printf("Thread %d\n", Thread.currentThread().getId());
         for (int i =0; i < max; i++) {
             synchronized (lock) {
                 counter += 1;
@@ -26,14 +21,16 @@ public class Counter implements Runnable {
         Thread[] threads = new Thread[THREADS];
         int i;
 
+        Counter c = new Counter();
+
         for (i=0; i< THREADS; i++) {
-            threads[i] = new Thread(new Counter(i));
+            threads[i] = new Thread(c);
             threads[i].start();
         }
 
         for (i=0; i< THREADS; i++) {
             threads[i].join();
         }
-        System.out.printf("Counter value: %d Expected: %d\n", counter, MAX_COUNT);
+        System.out.printf("Counter value: %d Expected: %d\n", c.counter, MAX_COUNT);
     }
 }
--- monitors/CounterMethod.java
+++ monitors/CounterMethod.java
@@ -1,22 +1,17 @@
 public class CounterMethod implements Runnable {
     static final int THREADS = 4;
     static final int MAX_COUNT = 10000000;
-    static volatile int counter = 0;
-    int id;
 
-    public CounterMethod(int id) {
-        this.id = id;
-    }
+    public volatile int counter = 0;
 
-    // It's static because every thread is a different instance
-    static synchronized void add() {
+    synchronized void add() {
         counter++;
     }
 
     @Override
     public void run() {
         int max = MAX_COUNT/THREADS;
-        System.out.printf("Thread %d\n", id);
+        System.out.printf("Thread %d\n", Thread.currentThread().getId());
         for (int i =0; i < max; i++) {
             add();
         }
@@ -26,14 +21,15 @@ public class CounterMethod implements Runnable {
         Thread[] threads = new Thread[THREADS];
         int i;
 
+        CounterMethod c = new CounterMethod();
         for (i=0; i< THREADS; i++) {
-            threads[i] = new Thread(new CounterMethod(i));
+            threads[i] = new Thread(c);
             threads[i].start();
         }
 
         for (i=0; i< THREADS; i++) {
             threads[i].join();
         }
-        System.out.printf("Counter value: %d Expected: %d\n", counter, MAX_COUNT);
+        System.out.printf("Counter value: %d Expected: %d\n", c.counter, MAX_COUNT);
     }
 }
