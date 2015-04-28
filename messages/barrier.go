cb2e485665f3fac41e0befed7118311d20b756e0
Ricardo Galli <gallir@gmail.com>
2015-04-28 03:54:01 +0200

    added barrier

---
+++ messages/barrier.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "runtime"
+    "time"
+)
+
+const (
+    PHASES     = 20
+    GOROUTINES = 10
+)
+
+type Empty struct{}
+
+type Barrier struct {
+    arrival, departure chan int
+    n                  int
+}
+
+func NewBarrier(value int) *Barrier {
+    b := new(Barrier)
+    b.arrival = make(chan int, 1)
+    b.departure = make(chan int, 1)
+    b.n = value
+
+    b.arrival <- value
+    return b
+}
+
+func (b *Barrier) Barrier() {
+    var v int
+
+    v = <-b.arrival
+    v--
+    if v > 0 {
+        b.arrival <- v
+    } else {
+        b.departure <- b.n
+    }
+
+    v = <-b.departure
+
+    v--
+    if v > 0 {
+        b.departure <- v
+    } else {
+        b.arrival <- b.n
+    }
+}
+
+func run(id int, done chan Empty, b *Barrier) {
+    for i := 0; i < PHASES; i++ {
+        time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+        b.Barrier()
+        fmt.Printf("%d finished phase %d\n", id, i)
+    }
+
+    b.Barrier()
+    fmt.Printf("Finished thread %d\n", id)
+
+    done <- Empty{}
+}
+
+func main() {
+    runtime.GOMAXPROCS(GOROUTINES)
+    done := make(chan Empty, 1)
+    barrier := NewBarrier(GOROUTINES)
+
+    for i := 0; i < GOROUTINES; i++ {
+        go run(i, done, barrier)
+    }
+
+    for i := 0; i < GOROUTINES; i++ {
+        <-done
+    }
+
+    fmt.Printf("End\n")
+}
