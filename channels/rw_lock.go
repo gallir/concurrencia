b10928c6a268c318b2b733c1decda18a88cff75f
Ricardo Galli <gallir@gmail.com>
2015-05-01 21:52:09 +0200

    RW Lock with messages

---
+++ channels/rw_lock.go
@@ -0,0 +1,94 @@
+/* Implements RW lock exclusion using messages */
+
+package main
+
+import (
+    "fmt"
+    "runtime"
+)
+
+const (
+    MAX_COUNT  = 10000000
+    GOROUTINES = 4
+)
+
+type Empty struct{}
+type RWLock struct {
+    writer chan Empty
+    readers chan int
+}
+
+func NewRWLock() RWLock {
+    var lock RWLock
+    lock.writer = make(chan Empty, 1)
+    lock.readers = make(chan int, 1)
+    lock.writer <- Empty{}
+    lock.readers <- 0
+    return lock
+}
+
+func (l RWLock) readerLock() {
+    readers := <- l.readers
+    readers++
+    if readers == 1 {
+        <- l.writer
+    }
+    l.readers <- readers
+    //fmt.Printf("reader in %d\n", readers)
+}
+
+func (l RWLock) readerUnlock() {
+    readers := <- l.readers
+    readers--
+    if readers == 0 {
+        l.writer <- Empty{}
+    }
+    l.readers <- readers
+    //fmt.Printf("reader out\n")
+
+}
+
+func (l RWLock) writerLock() {
+    <- l.writer
+    //fmt.Printf("writer in\n")
+}
+
+func (l RWLock) writerUnlock() {
+    l.writer <- Empty{}
+    //fmt.Printf("writer out\n")
+
+}
+
+var counter = 0
+
+func run(id, counts int, done chan Empty, rwlock RWLock) {
+    for i := 0; i < counts; i++ {
+        if i % 10 > 0 {
+            rwlock.readerLock()
+            // c := counter
+            rwlock.readerUnlock()
+        } else {
+            rwlock.writerLock()
+            counter++
+            rwlock.writerUnlock()
+        }
+    }
+    fmt.Printf("End %d counter: %d\n", id, counter)
+    done <- Empty{}
+}
+
+func main() {
+    runtime.GOMAXPROCS(GOROUTINES)
+    done := make(chan Empty, 1)
+    rwlock := NewRWLock()
+
+    for i := 0; i < GOROUTINES; i++ {
+        go run(i, MAX_COUNT/GOROUTINES, done, rwlock)
+    }
+
+    for i := 0; i < GOROUTINES; i++ {
+        <-done
+    }
+
+    fmt.Printf("Counter value: %d Expected: %d\n", counter, MAX_COUNT/10)
+}
