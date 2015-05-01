19a98554dc5ca1bc7eae20148ced5d0806d4a943
Ricardo Galli <gallir@gmail.com>
2015-05-02 00:01:43 +0200

    Added producer/consumer

---
+++ channels/producer_consumer.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "fmt"
+    "runtime"
+)
+
+const (
+    GOROUTINES = 4
+    TO_PRODUCE = 1000
+    PRODUCERS = 2
+    CONSUMERS = 2
+    BUFFER_SIZE = 10
+)
+
+type Empty struct{}
+
+func consumer(id int, done chan Empty, buffer chan string) {
+    for i := 0; i < TO_PRODUCE; i++ {
+        data := <- buffer
+        fmt.Printf("%d read: %s\n", id, data)
+    }
+    done <- Empty{}
+}
+
+func producer(id int, done chan Empty, buffer chan string) {
+    for i := 0; i < TO_PRODUCE; i++ {
+        data := fmt.Sprintf("%d i: %d", id, i)
+        buffer <- data
+    }
+}
+
+func main() {
+    runtime.GOMAXPROCS(GOROUTINES)
+    done := make(chan Empty, 1)
+    buffer := make(chan string, BUFFER_SIZE)
+
+    for i := 0; i < PRODUCERS; i++ {
+        go producer(i, done, buffer)
+    }
+
+    for i := 0; i < CONSUMERS; i++ {
+        go consumer(i, done, buffer)
+    }
+
+    for i := 0; i < CONSUMERS; i++ {
+        <-done
+    }
+
+    fmt.Printf("End\n")
+}
