46289a2dbe382a5d279f91da03b68f1ccb908c4b
Ricardo Galli <gallir@gmail.com>
2015-05-04 02:09:37 +0200

    Added 2 way barrier

---
+++ channels/2p_barrier.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "fmt"
+)
+
+const (
+    PHASES     = 20
+)
+
+
+func A(done, a, b chan bool) {
+    for i := 0; i < PHASES; i++ {
+        a <- true
+        fmt.Println("A finished phase", i)
+        <- b
+    }
+    fmt.Println("Finished thread A")
+    done <- true
+}
+
+func B(done, a, b chan bool) {
+    for i := 0; i < PHASES; i++ {
+        <- a
+        b <- true
+        fmt.Println("B finished phase", i)
+    }
+    fmt.Println("Finished thread B")
+    done <- true
+}
+
+
+func main() {
+    done := make(chan bool)
+    a := make(chan bool)
+    b := make(chan bool)
+
+    go A(done, a, b)
+    go B(done, a, b)
+
+    <- done
+    <- done
+
+    fmt.Printf("End\n")
+}
